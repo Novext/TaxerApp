d71c618d9e952c529968b1e7a3b60e3cf7400495
JULIO <julcsar13@gmail.com>
2016-12-30 08:28:04 -0500

    commit init

--- app/src/main/java/com/novext/taxerapp/LoginActivity.java
+++ app/src/main/java/com/novext/taxerapp/LoginActivity.java
@@ -3,11 +3,16 @@ package com.novext.taxerapp;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
     GoogleApiClient mGoogleApiClient;
@@ -31,4 +36,51 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
     }
+
+    loginButton = (LoginButton) findViewById(R.id.btnFacebook);
+
+    loginButton.setReadPermissions("email");
+
+
+
+    loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+        @Override
+        public void onSuccess(LoginResult loginResult) {
+            Toast.makeText(LoginActivity.this, loginResult.getAccessToken().getUserId(), Toast.LENGTH_SHORT).show();
+            Toast.makeText(LoginActivity.this, loginResult.getAccessToken().getToken(), Toast.LENGTH_SHORT).show();
+
+            final AccessToken accessToken = loginResult.getAccessToken();
+
+            final JSONObject obj = new JSONObject();
+            final OKHttp okHttp = new OKHttp();
+            final String url  = "https://dietapplication.herokuapp.com/api/users/social";
+
+            GraphRequestAsyncTask request_faccebook = GraphRequest.newMeRequest(accessToken, new GraphRequest.GraphJSONObjectCallback() {
+                @Override
+                public void onCompleted(JSONObject user, GraphResponse graphResponse) {
+
+
+                    try {
+                        obj.put("firstname",user.optString("name"));
+                        obj.put("lastname",user.optString("email"));
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).executeAsync();
+        }
+        @Override
+        public void onCancel() {
+
+        }
+        @Override
+        public void onError(FacebookException error) {
+            Log.e("AQUI ERROR 2", String.valueOf(error));
+        }
+    });
+    String txtGoogle = "Iniciar sesión con Google";
+    setGooglePlusButtonText(btnLoginGoogle,txtGoogle);
+}
+
 }
